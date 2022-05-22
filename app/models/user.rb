eb1bebc155da678bd86fe20d38b6b691daa7a7d4
Laurent <admin@jorune.co>
2022-05-22 12:30:55 +0200

    fix small issue

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < Sinatra::Base
   def self.get_all
     @data = {
       'Clement': { first_name: 'Clement', last_name: 'Denizet', age: 32 },
-      'Laurent':    { first_name: 'Laurent', last_name: 'Random', age: 47 },
-      'Vincent':     { first_name: 'Vincent', last_name: 'Smith', age: 30 }
+      'Laurent': { first_name: 'Laurent', last_name: 'Random', age: 47 },
+      'Vincent': { first_name: 'Vincent', last_name: 'Smith', age: 30 }
     }
   end
 
